b8bca2ce16c6002c5f7ff63c2350b850131fb6b2
VicSloana <vgarciajimenez01@saintmarys.edu>
2024-10-19 10:40:15 -0400

    Quiz 3.py

--- Quiz 3.py
+++ Quiz 3.py
@@ -5,53 +5,43 @@
 #    print("Oh, I am trap in a loop!")
 
 #2 Print te sum of all numbers from 1 to 10 using while loop 
-
-##i = 1
-##sum = 0
-##While i < 12:
-##    sum += i
-##    i += 1
-##print("The sum of all numbers from 1 to 12 is", sum)
+i = 1
+sum = 0
+While i < 12:
+    sum += i
+    i += 1
+print("The sum of all numbers from 1 to 12 is", sum)
 
 
 #3 A recursive function, named sum_n , to calculate the sum of all natural numbers from 1 to n
-
-##
-##def sum_n(n):
-##    n = sum(range(1, n))
-##    print(sum_n(n))
-##
-##sum_n(3)
-##
-##def fibonacci(n):
-##    if
-
+def sum_n(n):
+    n = sum(range(1, n))
+    print(sum_n(n))
+sum_n(3)
 
 #5
-##def tire_pressure(pres):
-##    if pres < 28:
-##        print("The tire is low")
-##    else:
-##        return None
-##
-##tire_pressure(22)
-##tire_pressure(55)
-##
-###6
-##def thermostat(temp):
-##    if temp <= 52:
-##        print("The heater is on")
-##    elif 52 < temp < 71:
-##        print("Heater and AC are off")
-##    elif temp >= 71:
-##        print("AC is on")
-##
-##thermostat(50)
-##thermostat(60)
-##thermostat(72)
+def tire_pressure(pres):
+    if pres < 28:
+        print("The tire is low")
+    else:
+       return None
+tire_pressure(22)
+tire_pressure(55)
+
+#6
+def thermostat(temp):
+    if temp <= 52:
+        print("The heater is on")
+    elif 52 < temp < 71:
+        print("Heater and AC are off")
+    elif temp >= 71:
+        print("AC is on")
+
+thermostat(50)
+thermostat(60)
+thermostat(72)
 
 #7
-
 def fun_fruit(fruit):
     if fruit == "Banana":
         print("Banana it is")
@@ -64,7 +54,6 @@ def fun_fruit(fruit):
     elif fruit == "Melon":
         print("You are one in a melon")
 
-    
 fun_fruit("Banana")
 fun_fruit("Cherry")
 fun_fruit("Apple")
